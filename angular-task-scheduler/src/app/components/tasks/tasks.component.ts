c3fb028f395ba467dc912cb15e51e00c51ea5e07
mariaborisenko <102270943+mariaborisenko@users.noreply.github.com>
2023-07-19 01:12:17 -0400

    adds json server,about component, add task component, add footer component, add task service, ui service

--- angular-task-scheduler/src/app/components/tasks/tasks.component.ts
+++ angular-task-scheduler/src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,6 @@
 import { Component,OnInit } from '@angular/core';
+import { TaskService } from 'src/app/services/task.service';
 import { Task } from '../../Task';
-import {TASKS} from '../../mock-tasks';
 
 @Component({
   selector: 'app-tasks',
@@ -10,9 +10,28 @@ import {TASKS} from '../../mock-tasks';
 
 export class TasksComponent implements OnInit {
 
-  tasks: Task[] = TASKS;
+  tasks: Task[] = [];
 
-  constructor () { }
+  constructor (private taskService: TaskService) { }
   
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
+  }
+
+  deleteTask(task: Task) {
+    this.taskService
+      .deleteTask(task)
+      .subscribe(
+        () => (this.tasks = this.tasks.filter((t) => t.id !== task.id))
+      );
+  }
+
+  toggleReminder(task: Task) {
+    task.reminder = !task.reminder;
+    this.taskService.updateTaskReminder(task).subscribe();
+  }
+
+  addTask(task: Task) {
+    this.taskService.addTask(task).subscribe((task) => this.tasks.push(task));
+  }
 }
